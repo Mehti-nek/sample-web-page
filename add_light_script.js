a18f16949550b3aa0a68c7e56a501b116b118ece
Mehti <Mahdi.nekuie@gmail.com>
2024-02-09 21:20:41 +0330

    commit

---
+++ add_light_script.js
@@ -0,0 +1,37 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    submitForm();
+});
+
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var deviceId = document.getElementById("device_id").value;
+    var subnetId = document.getElementById("subnet_id").value;
+    var channel = document.getElementById("channel").value;
+
+    // Check if any field is empty
+    if (name === '' || deviceId === '' || subnetId === '' || channel === '') {
+        alert('Please fill in all required fields');
+        return;
+    }
+
+    // Validate id, channel1, and channel2 fields
+    if (!isValidNumber(deviceId) || !isValidNumber(subnetId) || !isValidNumber(channel)) {
+        alert('ID, subnet ID, and Channel must be numbers between 1 to 255');
+        return;
+    }
+
+    var url = "/get?" +
+        "type=light" 
+        "&name=" + encodeURIComponent(name) +
+        "&device_id=" + encodeURIComponent(deviceId) +
+        "&subnet_id=" + encodeURIComponent(subnetId) +
+        "&channel=" + encodeURIComponent(channel)
+
+    window.location.href = url;
+}
+
+function isValidNumber(value) {
+    // Check if value is a number between 0 and 255
+    return !isNaN(value) && parseInt(value) >= 1 && parseInt(value) <= 255;
+}
--- index_script.js
+++ index_script.js
@@ -1,1 +1,10 @@
-// You can add JavaScript functionality here if needed
+function toggleMenu() {
+    var menuItems = document.getElementById("menuItems");
+    menuItems.classList.toggle("open");
+    if (menuItems.classList.contains("open")) {
+      menuItems.style.display = "block";
+    } else {
+      menuItems.style.display = "none";
+    }
+}
+  
